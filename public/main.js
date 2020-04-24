c6a6c0d22052a3aa71bce10807de4b14b50ebdd1
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-24 18:39:47 -0400

    created canvas element on the html page. created variable to hold the getContext method on the canvas. created variable to set the is drawing and x and y coordinates. created draw function. created event listeners for the mouse over mouse down mouse up and mouse out events

--- public/main.js
+++ public/main.js
@@ -1,7 +1,21 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const canvas = document.querySelector('#draw')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55'
+ctx.lineJoin = 'round'
+ctx.lineCap = 'round'
+
+let isDrawing = false
+let lastX = 0
+let lastY = 0
+
+function draw(event) {
+  if (!isDrawing) return // stop the function from running when they are not moused
+  console.log(event)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+canvas.addEventListener('mousemove', draw) 
+canvas.addEventListener('mousedown', () => isDrawing = true)
+canvas.addEventListener('mouseup', () => isDrawing = false)
+canvas.addEventListener('mouseout', () => isDrawing = false)
