9a86f791cb6ca6149894cf40da8f242c4016b26b
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-24 22:11:32 -0400

    added hue variable and incremented that in the draw function

--- public/main.js
+++ public/main.js
@@ -5,6 +5,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55'
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
+ctx.lineWidth = 100
 
 let isDrawing = false
 let lastX = 0
@@ -19,9 +20,13 @@ function draw(event) {
   // go to
   ctx.lineTo(event.offsetX, event.offsetY)
   ctx.stroke()
+  [lastX, lastY] = [event.offsetX, event.offsetY]
 }
 
+canvas.addEventListener('mousedown', (event) => {
+  isDrawing = true
+  [lastX, lastY] = [event.offsetX, event.offsetY]
+})
 canvas.addEventListener('mousemove', draw) 
-canvas.addEventListener('mousedown', () => isDrawing = true)
 canvas.addEventListener('mouseup', () => isDrawing = false)
-canvas.addEventListener('mouseout', () => isDrawing = false)
+canvas.addEventListener('mouseout', () => isDrawing = false) 
