d99cb4b3f8aa8d0dbea15352a6700e9c6c947ea2
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2021-02-16 19:22:41 -0500

    drew a triangle and then a happy face

--- public/main.js
+++ public/main.js
@@ -30,16 +30,42 @@ canvas.addEventListener('mouseup', (e) => {
 })
 
 
-/* ================================================================
-   Create a black box with a white box inside and then box outline
-   inside white box by following along with MDN
-   ================================================================
+/*
+
+================================================================
+ Draw a black box with a white box inside and then box outline
+ inside white box by following along with MDN
+================================================================
 
 ctx.fillRect(25, 25, 100, 100);
 ctx.clearRect(45, 45, 60, 60);
 ctx.strokeRect(50, 50, 50, 50);
 
+==================
+ Draw a triangle
+==================
+
+ctx.beginPath();
+    ctx.moveTo(75, 50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.fill();
+
+
+==================
+ Draw a triangle
+==================
 */
+
+ctx.beginPath();
+ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+ctx.moveTo(110, 75);
+ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+ctx.moveTo(65, 65);
+ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+ctx.moveTo(95, 65);
+ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+ctx.stroke();
 
 
 
